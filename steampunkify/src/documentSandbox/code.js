edb687783b0a83e4e856379f73ef7d1475cdb684
Avanish Kulkarni <bigfatnotebook@gmail.com>
2024-11-02 17:54:52 -0400

    wtf is a circle

--- steampunkify/src/documentSandbox/code.js
+++ steampunkify/src/documentSandbox/code.js
@@ -11,6 +11,7 @@ governing permissions and limitations under the License.
 */
 
 import addOnSandboxSdk from "add-on-sdk-document-sandbox";
+import { editor } from "express-document-sdk";
 const { runtime } = addOnSandboxSdk.instance;
 
 function start() {
@@ -18,6 +19,45 @@ function start() {
     log: (...args) => {
       console.log(...args);
     },
+    addGear({ teeth, innerRadius, outerRadius, angle }) {
+      console.log("addGear", teeth, innerRadius, outerRadius, angle);
+      // generate a gear...
+      const doc = editor.documentRoot;
+      const page = doc.pages.first;
+
+      var teethArr = [];
+      let c = editor.createEllipse();
+      c.width = innerRadius * 2;
+      c.height = c.width;
+      console.log("c dim", c.width, c.height)
+      // c.translation = {
+      //   x: page.width / 2 - c.width / 2, y: page.height / 2 - c.height / 2
+      // };
+
+      for (let i = 0; i < teeth; i++) {
+        let r = editor.createRectangle();
+        r.width = outerRadius - innerRadius;
+        r.height = 10;
+        let center_x = page.width / 2;
+        let center_y = page.height / 2
+
+        let angle = (i * (360 / teeth)) * (Math.PI / 180);
+        let radius = (c.width / 2) + 10;
+
+        r.translation = {
+          x: center_x + radius * Math.cos(angle) - r.width / 2,
+          y: center_y + radius * Math.sin(angle) - r.height / 2,
+        }
+        r.setRotationInParent(i * (360 / teeth), { x: r.width / 2, y: r.height / 2 })
+        teethArr.push(r);
+      }
+
+      page.artboards.first.children.append(c);
+      teethArr.forEach((rect) => page.artboards.first.children.append(rect));
+    },
+    delGear() {
+      console.log("delGear");
+    }
     // other properties will go here...
   });
 }
--- steampunkify/src/ui/index.js
+++ steampunkify/src/ui/index.js
@@ -19,12 +19,43 @@ import "@spectrum-web-components/theme/express/scale-medium.js";
 import "@spectrum-web-components/theme/sp-theme.js";
 
 import "@spectrum-web-components/button/sp-button.js";
+import "@spectrum-web-components/button-group/sp-button-group.js";
+import "@spectrum-web-components/field-label/sp-field-label.js";
+import "@spectrum-web-components/number-field/sp-number-field.js";
+import "@spectrum-web-components/slider/sp-slider.js";
+import "@spectrum-web-components/swatch/sp-swatch.js";
+
 
 import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
 
 addOnUISdk.ready.then(async () => {
   console.log("addOnUISdk is ready for use.");
-  const createShapeButton = document.getElementById("createShape");
+
+  // Input Fields
+  const teethInput = document.getElementById("teeth");
+  const innerRadiusInput = document.getElementById("inner-radius");
+  const outerRadiusInput = document.getElementById("outer-radius");
+  const angleInput = document.getElementById("angle");
+
+  teethInput.value = 32;
+  innerRadiusInput.value = 50;
+  outerRadiusInput.value = 60;
+  angleInput.value = 10;
+
+  const createGearButton = document.getElementById("createGear");
+  const deleteGearButton = document.getElementById("deleteGear");
+
+  createGearButton.onclick = async (event) => {
+    await sandboxProxy.addGear({
+      teeth: teethInput.value,
+      innerRadius: innerRadiusInput.value,
+      outerRadius: outerRadiusInput.value,
+      angle: angleInput.value
+    });
+  }
+  deleteGearButton.onclick = async (event) => {
+    await sandboxProxy.delGear();
+  }
 
   // Get the UI runtime.
   const { runtime } = addOnUISdk.instance;
@@ -32,5 +63,6 @@ addOnUISdk.ready.then(async () => {
   sandboxProxy.log("Document sandbox up and running.");
 
   // Enabling CTA elements only when the addOnUISdk is ready
-  createShapeButton.disabled = false;
+  createGearButton.disabled = false;
+  deleteGearButton.disabled = false;
 });
