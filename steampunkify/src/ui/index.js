b1a475410a91079ce0d36299ffda0878b72491a6
acabrera04 <acabrera04164@gmail.com>
2024-11-03 08:19:47 -0500

    currently working filters straight from document"

--- steampunkify/src/ui/index.js
+++ steampunkify/src/ui/index.js
@@ -31,6 +31,7 @@ import { fabric } from 'fabric';
 
 addOnUISdk.ready.then(async () => {
     console.log("addOnUISdk is ready for use.");
+    
     const createShapeButton = document.getElementById("createShape");
 
     // Get the UI runtime.
@@ -38,94 +39,84 @@ addOnUISdk.ready.then(async () => {
     const sandboxProxy = await runtime.apiProxy("documentSandbox");
     sandboxProxy.log("Document sandbox up and running.");
 
-    createShapeButton.addEventListener("click", async () => {
-        sandboxProxy.createShape({ width: 200, height: 100 }); //
-    })
-
-    // Enabling CTA elements only when the addOnUISdk is ready
-    createShapeButton.disabled = false;
-
-    const canvas = new fabric.Canvas('canvas');
+    const documentHeight = document.documentElement.scrollHeight;
+    const documentWidth = document.documentElement.scrollWidth;
+    const iframeWidth = window.innerWidth;
+    const canvas = new fabric.Canvas('canvas', {
+        width: iframeWidth,
+        height: iframeWidth * (documentHeight / documentWidth)
+    });
     
     let imageObject;
-
-    document.getElementById('uploadImage').addEventListener('change', (event) => {
-        const reader = new FileReader();
-        console.log("After Image inputted", event);
-        reader.onload = (e) => {
-            const dataURL = e.target.result;
-            // console.log("File reader result: ", dataURL);
-            fabric.Image.fromURL(dataURL, function(img) {
-                imageObject = img;
-                console.log("ImageObject updated");
-                img.set({
-                    left: 0,
-                    top: 0,
-                    scaleX: canvas.width / img.width,
-                    scaleY: canvas.height / img.height,
-                    crossOrigin: 'Anonymous'
-                },
-                {
-                    crossOrigin: 'Anonymous'
-                });
-                canvas.clear();
-                canvas.add(img);
-            });
-        };
-        reader.onerror = (error) => {
-            console.error("Error reading file:", error);
-        };
-        reader.readAsDataURL(event.target.files[0]);
-    });
-
-    document.getElementById('uploadButton').addEventListener('click', () => {
-        console.log("Upload Button clicked");
-        if (imageObject) {
-            console.log("Upload Button, image exists");
-            const filterType = document.getElementById('filter-drop').__selected;
-            imageObject.filters = [];
-            console.log("FilterTypes: ", filterType);
-            switch (filterType) {
-                case 'Sepia':
-                    imageObject.filters.push(new fabric.Image.filters.Sepia());
-                    break;
-                case 'Grayscale':
-                    imageObject.filters.push(new fabric.Image.filters.Grayscale());
-                    break;
-            }
-            console.log("Image Filters: ", imageObject.filters);
-            imageObject.applyFilters();
-            canvas.renderAll();
-        }
-        /* const dataURL = canvas.toDataURL({
-            format: 'png',
-            quality: 1
-        });
-        console.log("Data URL: ", dataURL);
-        
-        const link = document.getElementById('DownloadLink');
-        link.href = dataURL;
-        link.download = 'filtered-image.png';
-        console.log("Download Link: ", link);
-        link.addEventListener('click', (event) => {
-            console.log("Download Link clicked", event);
-        }); */
-    });
     
-    document.getElementById('downloadButton').addEventListener('click', async (event) => {
-        event.preventDefault();  // Prevent default behavior
-        console.log("Download Button clicked");
+    async function loadImage(downloadUrl) {
+        return new Promise((resolve, reject) => {
+            fabric.Image.fromURL(downloadUrl, function(img) {
+                if (img) {
+                    imageObject = img;
+                    console.log("ImageObject updated", imageObject);
+                    img.set({
+                        left: 0,
+                        top: 0,
+                        scaleX: canvas.width / img.width,
+                        scaleY: canvas.height / img.height,
+                        crossOrigin: 'Anonymous'
+                    },
+                    {
+                        crossOrigin: 'Anonymous'
+                    });
+                    canvas.clear();
+                    canvas.add(img);
+                    resolve();
+                } else {
+                    reject(new Error("Image loading failed"));
+                }
+            });
+        });
+    }
+    document.getElementById('applyButton').addEventListener('click', async () => {
+        
+        console.log("Upload Button, image exists");
         const response = await addOnUISdk.app.document.createRenditions({
             range: "currentPage",
             format: "image/jpeg",
         });
-        
+        const downloadUrl = URL.createObjectURL(response[0].blob);
+        await loadImage(downloadUrl);
+        const filterType = document.getElementById('filter-drop').__selected;
+        imageObject.filters = [];
+        console.log("FilterTypes: ", filterType);
+        switch (filterType) {
+            case 'Sepia':
+                imageObject.filters.push(new fabric.Image.filters.Sepia());
+                break;
+            case 'Grayscale':
+                imageObject.filters.push(new fabric.Image.filters.Grayscale());
+                break;
+        }
+        console.log("Image Filters: ", imageObject.filters);
+        imageObject.applyFilters();
+        canvas.renderAll();
+    });
+    
+
+    async function addImageFromURL(url) {
+        try {
+          const blob = await fetch(url).then((response) => response.blob());
+          await document.addImage(blob);
+        } catch (error) {
+          console.log("Failed to add the image to the page.");
+        }
+    }
+
+    document.getElementById('downloadButton').addEventListener('click', async (event) => {
+        event.preventDefault();  // Prevent default behavior
+        console.log("Download Button clicked");
+
         if (!imageObject) {
             console.error("No image object found for download.");
             return;  // Exit if there is no image to download
         }
-        imageObject.applyFilters();
-        canvas.renderAll();
         
         const dataURL = canvas.toDataURL({
             format: 'png',
@@ -133,12 +124,6 @@ addOnUISdk.ready.then(async () => {
         });
         console.log("Data URL: ", dataURL);
         
-        const link = document.createElement('a');
-        link.href = dataURL;
-        link.download = 'filtered-image.png';
-        document.body.appendChild(link);
-        link.click();
-        console.log("Download Link: ", link);
-        document.body.removeChild(link);
+        await addImageFromURL(dataURL);
     });
 });
